c657d3420b6a41537f6e671596dde0bf001cbb96
Diogo Emanuel <diogodeas1@gmail.com>
2023-06-24 14:34:00 -0300

    maquinaGenerica

--- testeMT.py
+++ testeMT.py
@@ -1,60 +1,44 @@
 import json
+import sys
 
-def run_turing_machine(mt, word):
-    tape = {0: {0: word}}  # Inicializa a fita na primeira trilha com a palavra fornecida
-    head = 0  # Posição inicial da cabeça de leitura/escrita
+def run_turing_machine(mt, palavra):
+    listPalavra = list(palavra)
+    listPalavra.insert(0, mt[4]) #insere >
+    listPalavra.append(mt[5]) # insere U
+    listTrilhas = [listPalavra]  # Inicializa a fita na primeira trilha com a palavra fornecida
+    tamanhotrilha = len(listPalavra)
+    trilhaGenerica = [mt[5]] * tamanhotrilha
+    for _ in range(mt[0]-1):
+        listTrilhas.append(trilhaGenerica)
+    cabecote = 1  # Posição inicial da cabeça de leitura/escrita
+    print(listTrilhas)
+    estados = mt[1]
+    maquinaEstados = mt[7]  # Estado inicial
 
-    current_state = mt['i']  # Estado inicial
-
+    transicoes = mt[6]
     while True:
-        # Verifica se o estado atual é um estado final
-        if current_state in mt['f']:
+    # Verifica se o estado atual é um estado final
+        if maquinaEstados in mt[8] #e transicao é invalida:
             return True  # A palavra pertence à linguagem
+        
+        #percorra a lista transicao
+        for transicao in transicoes: 
+            if transicao[0] == maquinaEstados:
+                
+        
 
-        # Obtém o símbolo atual sob a cabeça de leitura/escrita
-        current_symbol = tape[head].get(0, '_')
-
-        # Obtém a próxima transição com base no estado atual e símbolo atual
-        transitions = mt[6]
-        next_transition = None
-        for transition in transitions:
-            if transition[0] == current_state and transition[1] == current_symbol:
-                next_transition = transition
-                break
-
-        if next_transition is None:
-            return False  # A palavra não pertence à linguagem
-
-        # Atualiza o estado atual
-        current_state = next_transition[3]
-
-        # Atualiza o símbolo sob a cabeça de leitura/escrita
-        tape[head][0] = next_transition[4]
-
-        # Move a cabeça de leitura/escrita para a próxima posição
-        if next_transition[5] == '>':
-            head += 1
-        elif next_transition[5] == '<':
-            head -= 1
-
-        # Cria uma nova trilha se necessário
-        if head not in tape:
-            tape[head] = {}
+    
 
 def main():
-    # Obtém o caminho do arquivo JSON
-    json_file = input("Digite o caminho do arquivo JSON: ")
-
-    # Obtém a palavra de entrada
-    word = input("Digite a palavra de entrada: ")
-
+    argumento = sys.argv;
+    print (argumento);
     try:
         # Abre o arquivo JSON e carrega as informações da máquina de Turing
-        with open(json_file) as file:
-            mt = json.load(file)
+        with open(argumento[1]) as file:
+            mt = json.load(file)["mt"]
 
         # Executa a máquina de Turing com a palavra de entrada fornecida
-        result = run_turing_machine(mt, word)
+        result = run_turing_machine(mt, argumento[2])
 
         if result:
             print("Sim")
