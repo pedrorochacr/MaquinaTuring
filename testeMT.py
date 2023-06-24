a2a1dc1f3aef570a220c38774adeb5a1be41fa5c
Diogo Emanuel <diogodeas1@gmail.com>
2023-06-24 15:56:40 -0300

    quasePronto

--- testeMT.py
+++ testeMT.py
@@ -1,7 +1,17 @@
 import json
 import sys
+# Posição inicial da cabeça de leitura/escrita
+cabecote = 1
+listTrilhas = []
+maquinaEstados = 0
+mt = {}
 
-def run_turing_machine(mt, palavra):
+def run_turing_machine(palavra):
+    global listTrilhas
+    global cabecote
+    global maquinaEstados
+    global mt
+
     listPalavra = list(palavra)
     listPalavra.insert(0, mt[4]) #insere >
     listPalavra.append(mt[5]) # insere U
@@ -10,35 +20,67 @@ def run_turing_machine(mt, palavra):
     trilhaGenerica = [mt[5]] * tamanhotrilha
     for _ in range(mt[0]-1):
         listTrilhas.append(trilhaGenerica)
-    cabecote = 1  # Posição inicial da cabeça de leitura/escrita
-    print(listTrilhas)
-    estados = mt[1]
     maquinaEstados = mt[7]  # Estado inicial
 
     transicoes = mt[6]
     while True:
-    # Verifica se o estado atual é um estado final
-        if maquinaEstados in mt[8] #e transicao é invalida:
-            return True  # A palavra pertence à linguagem
-        
-        #percorra a lista transicao
+        tem_transicao = False
+        #percorra a lista transicao e a maquina de multiplas trilhas e compara se a trasicao é válida
         for transicao in transicoes: 
             if transicao[0] == maquinaEstados:
-                
-        
+                variavel = 0
+                for i in range(len(listTrilhas)):
+                    # print(listTrilhas[i], transicao)
+                    # print(listTrilhas[i][cabecote], transicao[i+1], "\n")
+                    if listTrilhas[i][cabecote] == transicao[i+1]:
+                        variavel += 1
+                    else: 
+                        break
+            if variavel == len(listTrilhas):
+                realizaTransicao(transicao)
+                tem_transicao = True
+                break
+        if tem_transicao == False:
+            break
+    return maquinaEstados in mt[8]
+   
+def adicionar_branco(): # Fita ilimitada à direita
+    global listTrilhas
+    global mt
 
+    for i in range(len(listTrilhas)):
+        listTrilhas[i].append(mt[5])
+
+def realizaTransicao(transicao):        
+    global listTrilhas
+    global cabecote
+    global maquinaEstados
+    for i in range(len(listTrilhas)):
+        listTrilhas[i][cabecote] = transicao[len(listTrilhas)+2+i]
+    if transicao[-1] == ">": 
+        cabecote += 1
+
+        if cabecote == len(listTrilhas):
+            adicionar_branco()
+    else:
+        cabecote -= 1
+    maquinaEstados = transicao[len(listTrilhas)+1]
     
-
 def main():
-    argumento = sys.argv;
-    print (argumento);
+    argumento = sys.argv
+    if len(argumento) != 3:
+        print("python3 testeMT.py [MT] [Word]")
+        exit()
+
+    print (argumento)
     try:
+        global mt
         # Abre o arquivo JSON e carrega as informações da máquina de Turing
         with open(argumento[1]) as file:
             mt = json.load(file)["mt"]
 
         # Executa a máquina de Turing com a palavra de entrada fornecida
-        result = run_turing_machine(mt, argumento[2])
+        result = run_turing_machine(argumento[2])
 
         if result:
             print("Sim")
